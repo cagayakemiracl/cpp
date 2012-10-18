b3e9e599c16d82059efddead78a04f16672e4ce5
cagayakemiracl <hirosnonno1182@gmail.com>
2012-10-18 17:59:22 +0900

    modified:   temperature.cpp

--- temperature.cpp
+++ temperature.cpp
@@ -4,13 +4,11 @@ using namespace std;
 int main(void)
 {
 	double temp, high_temp, low_temp, sum = 0;
-	int no_of_temps = 0;
-	while (cin >> temp) {
-		sum += temp;
+	int no_of_temps;
+	for (no_of_temps = 0; cin >> temp; ++no_of_temps, sum += temp) {
 		if (!no_of_temps)     high_temp = low_temp = temp;
 		if (temp > high_temp) high_temp = temp;
 		if (temp < low_temp)  low_temp  = temp;
-		++no_of_temps;
 	}
 	cout << "High temperature: "    << high_temp << endl
 		 << "Low temperature: "     << low_temp  << endl
